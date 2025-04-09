46c3f704d655b705f39f9801e0843fe1d8c129a7
Emmanuel Maton <ematon@c1r6p3.42lehavre.fr>
2025-04-09 16:35:25 +0200

    module01/ex02 done

---
+++ 1/ex02/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ematon <ematon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/06 21:37:00 by ematon            #+#    #+#             */
+/*   Updated: 2025/04/09 16:34:54 by ematon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string *stringPTR = &brain;
+	std::string& stringREF = brain;
+
+	std::cout << "..................String memory adress: "
+		<< &brain << std::endl;
+	std::cout << "Memory address pointed to by stringPTR: "
+		<< stringPTR << std::endl;
+	std::cout << "Memory address pointed to by stringREF: "
+		<< &stringREF << std::endl;
+
+	std::cout << ".................................String value: "
+		<< brain << std::endl;
+	std::cout << "Value of the variable pointed to by stringPTR: "
+		<< *stringPTR << std::endl;
+	std::cout << "Value of the variable pointed to by stringREF: "
+		<< stringREF << std::endl;
+	return (0);
+}
