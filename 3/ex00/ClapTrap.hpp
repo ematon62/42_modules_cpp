f01668379f1c65025e8622f0b1fb27bc8b2a853d
Emmanuel Maton <ematon@c1r6p14.42lehavre.fr>
2025-04-24 18:06:32 +0200

    module 03 ex00 done

---
+++ 3/ex00/ClapTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ematon <ematon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 17:19:16 by ematon            #+#    #+#             */
+/*   Updated: 2025/04/24 17:39:33 by ematon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#pragma once
+
+class ClapTrap
+{
+	public:
+		ClapTrap(const std::string& name);
+		ClapTrap(const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& other);
+		~ClapTrap();
+		
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	private:
+		std::string _name;
+		int			_hit_points;
+		int			_energy_points;
+		int			_attack_damage;
+		
+};
---
+++ 3/ex00/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ematon <ematon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 18:00:13 by ematon            #+#    #+#             */
+/*   Updated: 2025/04/24 18:05:03 by ematon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+	ClapTrap jerem("jeremy");
+	jerem.attack("jean pierre");
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+	jerem.takeDamage(1);
+}
