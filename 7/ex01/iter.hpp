abc13891d57aca23d3569b03fb6fe99acba3b845
Your Name <you@example.com>
2025-07-01 13:47:07 +0200

    07 done?

--- 7/ex01/iter.hpp
+++ 7/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: gross <gross@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 14:42:51 by gross             #+#    #+#             */
-/*   Updated: 2025/06/30 21:30:29 by gross            ###   ########.fr       */
+/*   Updated: 2025/07/01 13:45:51 by gross            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,11 @@
 
 #include <iostream>
 
-template<typename T>
-void print(T& x)
+template <typename T>
+void iter(T* arr, size_t size, void (*f)(const T &))
 {
-    std::cout << x << std::endl;
-}
-
-template<typename T>
-void print2(const T& x)
-{
-    std::cout << x << std::endl;
-}
-
-template<typename T>
-void increment(T& x)
-{
-	x += 1;
-}
-
-template<typename T>
-void iter(T *array, size_t n, void (*f)(T& x))
-{
-    for (size_t i = 0; i < n; i++)
-    {
-        f(array[i]);
-    }
+    for (size_t i = 0; i < size; i++)
+		f(arr[i]);
 }
 
 #endif
--- 7/ex01/main.cpp
+++ 7/ex01/main.cpp
@@ -6,27 +6,44 @@
 /*   By: gross <gross@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 13:37:45 by gross             #+#    #+#             */
-/*   Updated: 2025/06/30 21:30:16 by gross            ###   ########.fr       */
+/*   Updated: 2025/07/01 13:45:45 by gross            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+int a = 2;
+
+template<typename T>
+void change_value(T& x)
+{
+	a = *x * 2;
+}
+
+template<typename T>
+void print(const T& x)
+{
+    std::cout << x << std::endl;
+}
+
 int main(void)
 {
 	{
 		const std::string array[] = {"caca", "pipi"};
-		iter(array, 2, &print);
+		iter(array, 2, print);
 		std::cout << std::endl;
-		iter(array, 2, &print2);
-
 	}
 	std::cout << std::endl;
 	{
-		int int_array[1] = { 1 };
-		iter(int_array, 4, &increment);
-		iter(int_array, 1, &print);
-		iter(int_array, 1, &print);
-
+		std::string array[] = {"caca", "pipi"};
+		iter(array, 2, print);
 	}
+	std::cout << std::endl;
+	{
+		int* array[1];
+		array[0] = &a;
+		iter(array, 1, change_value);
+	}
+
+	std::cout << a << std::endl;
 }
